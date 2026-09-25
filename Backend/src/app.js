2aa820c31bcbabb9dc2749d4c22efffc04888bc1
Rahul Sharma <rahulsharma80032@gmail.com>
2026-09-25 15:45:30 +0530

    cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,26 +1,31 @@
 import express from "express";
 import mongoose from "mongoose";
 import { createServer } from "node:http";
-import { Server } from "socket.io";
 import dotenv from "dotenv";
-import connectToSocket from "./controllers/socketManager.js";
 import cors from "cors";
-import { User } from "./models/userSchema.js";
+
+import connectToSocket from "./controllers/socketManager.js";
 import userRoutes from "./routes/user.js";
+
 dotenv.config();
+
+console.log("FRONTEND_URL:", process.env.FRONTEND_URL);
+
 const app = express();
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+
 const allowedOrigins = [
-    process.env.FRONTEND_URL,
-    "http://localhost:5173",
-    "http://localhost:5174",
+  process.env.FRONTEND_URL,
+  "http://localhost:5173"
 ].filter(Boolean);
 
 app.use(cors({
-    origin: allowedOrigins,
-    credentials: true,
+  origin: allowedOrigins,
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  credentials: true
 }));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use("/users",userRoutes);
 
 const server = createServer(app); // HTTP server banaya
--- Backend/src/controllers/socketManager.js
+++ Backend/src/controllers/socketManager.js
@@ -6,9 +6,9 @@ import { getTokenFromCookie } from "../middleware/auth.js";
 
 const connectToSocket = (server) => {
   const allowedOrigins = [
-    process.env.FRONTEND_URL,
-    "http://localhost:5173",
-    "http://localhost:5174",
+    process.env.FRONTEND_URL
+    // "http://localhost:5173",
+    // "http://localhost:5174",
   ].filter(Boolean);
 
   const io = new Server(server, {
